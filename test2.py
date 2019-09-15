d44fe738842220791dee21bd9b172399e5e76fd0
unknown <chungsan@hotmail.com>
2019-09-15 15:29:16 -0700

    put unwanted .visstudio to .gitignore

--- test2.py
+++ test2.py
@@ -1,6 +1,7 @@
 print("------------- added by Master');
 print("Hello");
 print("this is test2.py");
+print("changed more again 3:30pm");
 print("added more lines");
 print('bye bye')
 print("=============== added by Young");

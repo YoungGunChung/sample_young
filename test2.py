c88466216bffa27f706203082b28ebc39eb90c37
YoungGunChung <40170658+YoungGunChung@users.noreply.github.com>
2019-09-15 16:30:15 -0700

    Update test2.py
    bye young

--- test2.py
+++ test2.py
@@ -2,5 +2,5 @@ print("------------- added by Master');
 print("Hello");
 print("this is test2.py");
 print("added more lines");
-print('bye bye')
+print('bye bye Young')
 print("=============== added by Young");
